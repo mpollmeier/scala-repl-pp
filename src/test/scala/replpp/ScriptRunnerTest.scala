e1902a6e342aae0fc6beba1ac27c71e5c15da101
Michael Pollmeier <michael@michaelpollmeier.com>
2022-12-01 15:00:21 +0100

    refactor tests

--- src/test/scala/replpp/ScriptRunnerTest.scala
+++ src/test/scala/replpp/ScriptRunnerTest.scala
@@ -6,97 +6,107 @@ import org.scalatest.wordspec.AnyWordSpec
 class ScriptRunnerTest extends AnyWordSpec with Matchers {
 
   "execute simple single-statement script" in {
-    val (testOutputFile, testOutputPath) = newTempFileWithPath
-    exec(s"""os.write.over(os.Path("$testOutputPath"), "iwashere-simple")""")
-    os.read(testOutputFile) shouldBe "iwashere-simple"
+    execTest { testOutputPath =>
+      TestSetup(s"""os.write.over(os.Path("$testOutputPath"), "iwashere-simple")""")
+    } shouldBe "iwashere-simple"
   }
 
   "main entry point" in {
-    val (testOutputFile, testOutputPath) = newTempFileWithPath
-    exec(
-      s"""@main def foo() = {
-         |  os.write.over(os.Path("$testOutputPath"), "iwashere-@main")
-         |}""".stripMargin)
-
-    os.read(testOutputFile) shouldBe "iwashere-@main"
+    execTest { testOutputPath =>
+      TestSetup(
+        s"""@main def foo() = {
+           |  os.write.over(os.Path("$testOutputPath"), "iwashere-@main")
+           |}""".stripMargin
+      )
+    } shouldBe "iwashere-@main"
   }
 
   "multiple main entry points" in {
-    val (testOutputFile, testOutputPath) = newTempFileWithPath
-    exec(
-      s"""@main def foo() = {
-         |  os.write.append(os.Path("$testOutputPath"), "iwashere-@main-foo")
-         |}
-         |
-         |@main def bar() = {
-         |  os.write.append(os.Path("$testOutputPath"), "iwashere-@main-bar")
-         |}""".stripMargin,
-      adaptConfig = _.copy(command = Some("bar"))
-    )
-
-    os.read(testOutputFile) shouldBe "iwashere-@main-bar"
+    execTest { testOutputPath =>
+      TestSetup(
+        s"""@main def foo() = {
+           |  os.write.append(os.Path("$testOutputPath"), "iwashere-@main-foo")
+           |}
+           |
+           |@main def bar() = {
+           |  os.write.append(os.Path("$testOutputPath"), "iwashere-@main-bar")
+           |}""".stripMargin,
+        adaptConfig = _.copy(command = Some("bar"))
+      )
+    } shouldBe "iwashere-@main-bar"
   }
 
   "parameters" in {
-    val (testOutputFile, testOutputPath) = newTempFileWithPath
-    exec(
-      s"""@main def foo(value: String) = {
-         |  os.write.over(os.Path("$testOutputPath"), value)
-         |}""".stripMargin,
-      adaptConfig = _.copy(params = Map("value" -> "iwashere-parameter"))
-    )
-
-    os.read(testOutputFile) shouldBe "iwashere-parameter"
+    execTest { testOutputPath =>
+      TestSetup(
+        s"""@main def foo(value: String) = {
+           |  os.write.over(os.Path("$testOutputPath"), value)
+           |}""".stripMargin,
+        adaptConfig = _.copy(params = Map("value" -> "iwashere-parameter"))
+      )
+    } shouldBe "iwashere-parameter"
   }
 
   "--predefCode" in {
-    val (testOutputFile, testOutputPath) = newTempFileWithPath
-    exec(
-      s"""@main def foo() = {
-         |  os.write.over(os.Path("$testOutputPath"), bar)
-         |}""".stripMargin,
-      adaptConfig = _.copy(predefCode = Some("val bar = \"iwashere-predefCode\""))
-    )
-
-    os.read(testOutputFile) shouldBe "iwashere-predefCode"
+    execTest { testOutputPath =>
+      TestSetup(
+        s"""@main def foo() = {
+           |  os.write.over(os.Path("$testOutputPath"), bar)
+           |}""".stripMargin,
+        adaptConfig = _.copy(predefCode = Some("val bar = \"iwashere-predefCode\""))
+      )
+    } shouldBe "iwashere-predefCode"
   }
 
   "--predefFiles" in {
-    val (testOutputFile, testOutputPath) = newTempFileWithPath
-    val predefFile = os.temp()
-    os.write.over(predefFile, "val bar = \"iwashere-predefFile\"")
-    exec(
-      s"""@main def foo() = {
-         |  os.write.over(os.Path("$testOutputPath"), bar)
-         |}""".stripMargin,
+    execTest { testOutputPath =>
+      val predefFile = os.temp()
+      os.write.over(predefFile, "val bar = \"iwashere-predefFile\"")
+      TestSetup(
+        s"""@main def foo() = {
+           |  os.write.over(os.Path("$testOutputPath"), bar)
+           |}""".stripMargin,
         adaptConfig = _.copy(predefFiles = List(predefFile))
-    )
-
-    os.read(testOutputFile) shouldBe "iwashere-predefFile"
+      )
+    } shouldBe "iwashere-predefFile"
   }
 
   "additional dependencies via --dependency" in {
-    val (testOutputFile, testOutputPath) = newTempFileWithPath
-    exec(
-      s"""val compareResult = versionsort.VersionHelper.compare("1.0", "0.9")
-         |os.write.over(os.Path("$testOutputPath"), "iwashere-dependency0:" + compareResult)
-         |""".stripMargin,
+    execTest { testOutputPath =>
+      TestSetup(
+        s"""val compareResult = versionsort.VersionHelper.compare("1.0", "0.9")
+           |os.write.over(os.Path("$testOutputPath"), "iwashere-dependency0:" + compareResult)
+           |""".stripMargin,
         adaptConfig = _.copy(dependencies = Seq("com.michaelpollmeier:versionsort:1.0.7"))
-    )
-
-    os.read(testOutputFile) shouldBe "iwashere-dependency0:1"
+      )
+    } shouldBe "iwashere-dependency0:1"
   }
 
-  private def exec(scriptSrc: String, adaptConfig: Config => Config = identity): Unit = {
+  "additional dependencies via --dependency 2" in {
+    execTest { testOutputPath =>
+      TestSetup(
+        s"""val compareResult = versionsort.VersionHelper.compare("1.0", "0.9")
+           |os.write.over(os.Path("$testOutputPath"), "iwashere-dependency0:" + compareResult)
+           |""".stripMargin,
+        adaptConfig = _.copy(dependencies = Seq("com.michaelpollmeier:versionsort:1.0.7"))
+      )
+    } shouldBe "iwashere-dependency0:1"
+  }
+
+  type TestOutputPath = String
+  type TestOutput = String
+  case class TestSetup(scriptSrc: String, adaptConfig: Config => Config = identity)
+  private def execTest(setupTest: TestOutputPath => TestSetup): TestOutput = {
+    val testOutputFile = os.temp()
+    val testOutputPath = testOutputFile.toNIO.toAbsolutePath.toString
+
+    val TestSetup(scriptSrc, adaptConfig) = setupTest(testOutputPath)
     val scriptFile = os.temp()
     os.write.over(scriptFile, scriptSrc)
     val config = adaptConfig(Config(scriptFile = Some(scriptFile)))
     ScriptRunner.exec(config)
-  }
 
-  private def newTempFileWithPath: (os.Path, String) = {
-    val tmpFile = os.temp()
-    (tmpFile, tmpFile.toNIO.toAbsolutePath.toString)
+    os.read(testOutputFile)
   }
 
 }
