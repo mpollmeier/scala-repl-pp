04dd1f8c29f4cdcae16ae1e690b24aad4e7949d3
Michael Pollmeier <michael@michaelpollmeier.com>
2022-12-05 19:06:47 +0100

    WIP

--- src/main/scala/replpp/ReplDriver.scala
+++ src/main/scala/replpp/ReplDriver.scala
@@ -38,40 +38,75 @@ class ReplDriver(args: Array[String],
       override protected def promptStr = prompt
     }
     initializeRenderer()
-
     greeting.foreach(out.println)
 
-    /** Blockingly read a line, getting back a parse result */
-    def readLine(state: State): ParseResult = {
-      val completer: Completer = { (_, line, candidates) =>
-        val comps = completions(line.cursor, line.line, state)
-        candidates.addAll(comps.asJava)
-      }
-      given Context = state.context
-      try {
-        val lines = terminal.readLine(completer).split(lineSeparator)
-        val linesFinal = substituteFileImportsWithContents(lines)
-
-        ParseResult(linesFinal.mkString(lineSeparator))(using state)
-      } catch {
-        case _: EndOfFileException => // Ctrl+D
-          onExitCode.foreach(code => run(code)(using state))
-          Quit
-        case _: UserInterruptException => // Ctrl+C
-          Newline
-      }
-    }
-
     @tailrec def loop(using state: State)(): State = {
-      val res = readLine(state)
-      if (res == Quit) state
-      else loop(using interpret(res))()
+      val (parseResult, remainder) = readLine(terminal, state)
+      if (parseResult == Quit) state
+      else loop(using interpret(parseResult))()
     }
 
     try runBody { loop(using initialState)() }
     finally terminal.close()
   }
-  
+
+  private val completer: Completer = { (_, line, candidates) =>
+    val comps = completions(line.cursor, line.line, state)
+    candidates.addAll(comps.asJava)
+  }
+
+  /** Blockingly read a line, getting back a parse result */
+  def readLine(terminal: JLineTerminal, state: State): (ParseResult, Iterator[String]) = {
+    given Context = state.context
+    try {
+      val linesIter = terminal.readLine(completer).split(lineSeparator).iterator
+      val linesBeforeUsingFileDirectiveBuilder = Seq.newBuilder[String]
+      while (linesIter.hasNext) {
+        val line = linesIter.next()
+        if (line.trim.startsWith(UsingDirectives.FileDirective)) {
+          val linesBeforeUsingFileDirective = linesBeforeUsingFileDirectiveBuilder.result()
+          linesBeforeUsingFileDirectiveBuilder.clear()
+          // TODO interpret everything until here, then recursively call parseLine with content of file, then continue with the remainder of the linerIterator
+          ???
+        }
+
+      }
+//      val linesBeforeUsingFileDirective = linesIter.takeWhile(!_.trim.startsWith(UsingDirectives.FileDirective)).toArray
+      //        val linesFinal = substituteFileImportsWithContents(lines)
+      //
+      //        ParseResult(linesFinal.mkString(lineSeparator))(using state)
+      ???
+    } catch {
+      case _: EndOfFileException => // Ctrl+D
+        onExitCode.foreach(code => run(code)(using state))
+        Quit
+      case _: UserInterruptException => // Ctrl+C
+        Newline
+    }
+  }
+
+//  /**
+//    * for each `//> using file abc.sc` in the given `lines`, substitute it with the referenced file
+//    * this works recursively, i.e. if the referenced file references further files, those will be read as well
+//    */
+//  private def substituteFileImportsWithContents(lines: IterableOnce[String]): Seq[String] = {
+//    val trimmed = inputLine.trim
+//    if (trimmed.startsWith(UsingDirectives.LibDirective)) {
+//      out.println(s"warning: `using lib` directive does not work as input in interactive REPL - please pass it via predef code or `--dependency` list instead")
+//      Nil
+//    } else if (trimmed.startsWith(UsingDirectives.FileDirective)) {
+//      val files = UsingDirectives.findImportedFilesRecursively(inputLine.split(lineSeparator))
+//      files.toSeq.flatMap { file =>
+//        val ret = os.read.lines(file)
+//        out.println(s"read $file (${ret.size} lines)")
+//        ret
+//      }
+//    } else {
+//      Nil
+//    }
+//  }
+//
+
   /** configure rendering to use our pprinter for displaying results */
   private def initializeRenderer() = {
     rendering.myReplStringOf = {
@@ -83,27 +118,5 @@ class ReplDriver(args: Array[String],
       (value: Object) => renderer.invoke(null, value).asInstanceOf[String]
     }
   }
-
-  /**
-    * for each `//> using file abc.sc` in the given `lines`, substitute it with the referenced file
-    * this works recursively, i.e. if the referenced file references further files, those will be read as well
-    */
-  private def substituteFileImportsWithContents(lines: IterableOnce[String]): Seq[String] = {
-    val trimmed = inputLine.trim
-    if (trimmed.startsWith(UsingDirectives.LibDirective)) {
-      out.println(s"warning: `using lib` directive does not work as input in interactive REPL - please pass it via predef code or `--dependency` list instead")
-      Nil
-    } else if (trimmed.startsWith(UsingDirectives.FileDirective)) {
-      val files = UsingDirectives.findImportedFilesRecursively(inputLine.split(lineSeparator))
-      files.toSeq.flatMap { file =>
-        val ret = os.read.lines(file)
-        out.println(s"read $file (${ret.size} lines)")
-        ret
-      }
-    } else {
-      Nil
-    }
-  }
-
 
 }
