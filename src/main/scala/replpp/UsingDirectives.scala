159016aec1f7270f4989914b8eaf46d266ee8d14
Michael Pollmeier <michael@michaelpollmeier.com>
2022-12-05 19:10:37 +0100

    WIP

--- src/main/scala/replpp/UsingDirectives.scala
+++ src/main/scala/replpp/UsingDirectives.scala
@@ -8,14 +8,18 @@ object UsingDirectives {
   val LibDirective = s"$Prefix lib "
   val FileDirective = s"$Prefix file "
 
-  def findImportedFilesRecursively(file: os.Path, visited: Set[os.Path] = Set.empty): Set[os.Path] = {
-    val files = scanFor(FileDirective, os.read.lines(file)).iterator.map { pathStr =>
-      if (Path.of(pathStr).isAbsolute) os.Path(pathStr)
-      else file / os.RelPath(pathStr)
-    }.toSet
-    files ++ files.filterNot(visited.contains).flatMap { file =>
-      findImportedFilesRecursively(file, visited + file)
+  def findImportedFilesRecursively(file: os.Path): Set[os.Path] = {
+    def findImportedFilesRecursively0(file: os.Path, visited: Set[os.Path]): Set[os.Path] = {
+      val files = scanFor(FileDirective, os.read.lines(file)).iterator.map { pathStr =>
+        if (Path.of(pathStr).isAbsolute) os.Path(pathStr)
+        else file / os.RelPath(pathStr)
+      }.toSet
+      files ++ files.filterNot(visited.contains).flatMap { file =>
+        findImportedFilesRecursively0(file, visited + file)
+      }
     }
+
+    findImportedFilesRecursively0(file, visited = Set.empty)
   }
 
   def findDeclaredDependencies(source: String): IterableOnce[String] =
