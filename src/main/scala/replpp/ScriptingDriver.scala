a161e9b4b0ee15a9f5ba00058dfb01845ba06df2
Michael Pollmeier <michael@michaelpollmeier.com>
2022-12-08 16:43:12 +0100

    braces ftw and refactor for readability

--- src/main/scala/replpp/ScriptingDriver.scala
+++ src/main/scala/replpp/ScriptingDriver.scala
@@ -10,11 +10,12 @@ import dotty.tools.dotc.core.Contexts, Contexts.{ Context, ctx }
 import dotty.tools.io.{ PlainDirectory, Directory, ClassPath }
 import dotty.tools.scripting.Util.*
 
-class ScriptingDriver(compilerArgs: Array[String], scriptFile: File, scriptArgs: Array[String]) extends Driver:
-  def compileAndRun(pack:(Path, Seq[Path], String) => Boolean = null): Option[Throwable] =
+class ScriptingDriver(compilerArgs: Array[String], scriptFile: File, scriptArgs: Array[String]) extends Driver {
+  def compileAndRun(pack: (Path, Seq[Path], String) => Boolean = null): Option[Throwable] = {
     val outDir = Files.createTempDirectory("scala3-scripting")
     outDir.toFile.deleteOnExit()
-    setup(compilerArgs :+ scriptFile.getAbsolutePath, initCtx.fresh) match
+    
+    setup(compilerArgs :+ scriptFile.getAbsolutePath, initCtx.fresh) match {
       case Some((toCompile, rootCtx)) =>
         given Context = rootCtx.fresh.setSetting(rootCtx.settings.outputDir,
           new PlainDirectory(Directory(outDir)))
@@ -22,9 +23,11 @@ class ScriptingDriver(compilerArgs: Array[String], scriptFile: File, scriptArgs:
         if doCompile(newCompiler, toCompile).hasErrors then
           Some(ScriptingException("Errors encountered during compilation"))
         else
-          try
+          try {
             val classpath = s"${ctx.settings.classpath.value}${pathsep}${sys.props("java.class.path")}"
-            val classpathEntries: Seq[Path] = ClassPath.expandPath(classpath, expandStar=true).map { Paths.get(_) }
+            val classpathEntries: Seq[Path] = ClassPath.expandPath(classpath, expandStar = true).map {
+              Paths.get(_)
+            }
             detectMainClassAndMethod(outDir, classpathEntries, scriptFile.toString) match
               case Right((mainClass, mainMethod)) =>
                 val invokeMain: Boolean = Option(pack).map { func =>
@@ -33,14 +36,14 @@ class ScriptingDriver(compilerArgs: Array[String], scriptFile: File, scriptArgs:
                 if invokeMain then mainMethod.invoke(null, scriptArgs)
                 None
               case Left(ex) => Some(ex)
-          catch
+          } catch {
             case e: java.lang.reflect.InvocationTargetException =>
               Some(e.getCause)
-          finally
-            deleteFile(outDir.toFile)
+          } finally deleteFile(outDir.toFile)
       case None => None
-  end compileAndRun
+    }
+  }
 
-end ScriptingDriver
+}
 
 case class ScriptingException(msg: String) extends RuntimeException(msg)
