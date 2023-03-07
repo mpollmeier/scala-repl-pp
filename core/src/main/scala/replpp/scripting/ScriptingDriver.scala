12b0bb7f42234969a2e93c776065eb468d063c46
Michael Pollmeier <michael@michaelpollmeier.com>
2023-03-07 10:48:32 +0100

    'doCompile' only works without `mainargs`

--- core/src/main/scala/replpp/scripting/ScriptingDriver.scala
+++ core/src/main/scala/replpp/scripting/ScriptingDriver.scala
@@ -31,47 +31,17 @@ class ScriptingDriver(compilerArgs: Array[String], scriptFile: File, scriptArgs:
     setup(compilerArgs :+ scriptFile.getAbsolutePath, initCtx.fresh).flatMap { case (toCompile, rootCtx) =>
       val outDir = os.temp.dir(prefix = "scala3-scripting", deleteOnExit = false)
 
-//      // TODO cleanup
-//      val classpath0: String = rootCtx.settings.classpath.value(using rootCtx)
-//      val classloaderUrls = classOf[replpp.ReplDriver].getClassLoader.asInstanceOf[java.net.URLClassLoader].getURLs.mkString(pathSeparator)
-//      val classpath1 = s"$classpath0$pathSeparator$classloaderUrls$pathSeparator${sys.props("java.class.path")}"
-////      println("XXX1 cp=" + classpath1)
-//      val ctx = rootCtx.fresh
-//        .setSetting(rootCtx.settings.outputDir, new PlainDirectory(Directory(outDir.toNIO)))
-//        .setSetting(rootCtx.settings.classpath, classpath1)
-//      given Context = ctx
-// TODO enable debugging options for `--verbose`
+      // TODO enable debugging options only for `--verbose`
       val ctx = rootCtx.fresh
-//        .setSetting(rootCtx.settings.extdirs, "")
-//        .setSetting(rootCtx.settings.extdirs, "/home/mp/Projects/scala-repl-pp/lib-staged-works")
-//        .setSetting(rootCtx.settings.javaextdirs, "/home/mp/Projects/scala-repl-pp/lib-staged-works")
         .setSetting(rootCtx.settings.help, true)
         .setSetting(rootCtx.settings.XshowPhases, true)
         .setSetting(rootCtx.settings.Vhelp, true)
         .setSetting(rootCtx.settings.Vprofile, true)
         .setSetting(rootCtx.settings.explain, true)
-//        .setSetting(rootCtx.settings.usejavacp, true)
         .setSetting(rootCtx.settings.outputDir, new PlainDirectory(Directory(outDir.toNIO)))
-//        .setSetting(rootCtx.settings.classpath, "")
-        .setSetting(rootCtx.settings.classpath, "/home/mp/Projects/scala-repl-pp/lib-staged-works/com.michaelpollmeier.scala-repl-pp-all-0.0.34+4-b3cd989c+20230306-1016.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.michaelpollmeier.scala-repl-pp-0.0.34+4-b3cd989c+20230306-1016.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.michaelpollmeier.scala-repl-pp-server-0.0.34+4-b3cd989c+20230306-1016.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.scala3-library_3-3.3.0-RC3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.scala3-compiler_3-3.3.0-RC3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.mainargs_3-0.3.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.os-lib_3-0.8.1.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.pprint_3-0.8.1.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.github.scopt.scopt_3-4.1.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.get-coursier.coursier_2.13-2.0.16.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.modules.scala-xml_3-2.1.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.cask_3-0.8.3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.scala-library-2.13.10.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.scala3-interfaces-3.3.0-RC3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.tasty-core_3-3.3.0-RC3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.modules.scala-asm-9.4.0-scala-1.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-sbt.compiler-interface-1.3.5.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.jline.jline-reader-3.19.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.jline.jline-terminal-3.19.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.jline.jline-terminal-jna-3.19.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.modules.scala-collection-compat_3-2.8.1.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.geny_3-0.7.1.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.fansi_3-0.4.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.sourcecode_3-0.3.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.get-coursier.coursier-core_2.13-2.0.16.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.get-coursier.coursier-cache_2.13-2.0.16.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.github.alexarchambault.argonaut-shapeless_6.2_2.13-1.2.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.undertow.undertow-core-2.2.3.Final.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.upickle_3-1.6.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.cask-util_3-0.8.3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.google.protobuf.protobuf-java-3.7.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-sbt.util-interface-1.3.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/net.java.dev.jna.jna-5.3.1.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.get-coursier.coursier-util_2.13-2.0.16.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.github.alexarchambault.concurrent-reference-hash-map-1.0.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.github.alexarchambault.windows-ansi.windows-ansi-0.0.3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.argonaut.argonaut_2.13-6.2.5.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.chuusai.shapeless_2.13-2.3.3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.jboss.logging.jboss-logging-3.4.1.Final.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.jboss.xnio.xnio-api-3.8.0.Final.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.jboss.xnio.xnio-nio-3.8.0.Final.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.jboss.threads.jboss-threads-3.1.0.Final.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.ujson_3-1.6.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.upack_3-1.6.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.upickle-implicits_3-1.6.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.castor_3-0.1.8.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.java-websocket.Java-WebSocket-1.5.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.fusesource.jansi.jansi-1.18.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.scala-reflect-2.13.1.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.wildfly.common.wildfly-common-1.5.2.Final.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.wildfly.client.wildfly-client-config-1.0.1.Final.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.upickle-core_3-1.6.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.slf4j.slf4j-api-1.7.25.jar")
+//        .setSetting(rootCtx.settings.classpath, "/home/mp/Projects/scala-repl-pp/lib-staged-works/com.michaelpollmeier.scala-repl-pp-all-0.0.34+4-b3cd989c+20230306-1016.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.michaelpollmeier.scala-repl-pp-0.0.34+4-b3cd989c+20230306-1016.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.michaelpollmeier.scala-repl-pp-server-0.0.34+4-b3cd989c+20230306-1016.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.scala3-library_3-3.3.0-RC3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.scala3-compiler_3-3.3.0-RC3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.mainargs_3-0.3.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.os-lib_3-0.8.1.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.pprint_3-0.8.1.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.github.scopt.scopt_3-4.1.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.get-coursier.coursier_2.13-2.0.16.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.modules.scala-xml_3-2.1.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.cask_3-0.8.3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.scala-library-2.13.10.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.scala3-interfaces-3.3.0-RC3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.tasty-core_3-3.3.0-RC3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.modules.scala-asm-9.4.0-scala-1.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-sbt.compiler-interface-1.3.5.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.jline.jline-reader-3.19.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.jline.jline-terminal-3.19.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.jline.jline-terminal-jna-3.19.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.modules.scala-collection-compat_3-2.8.1.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.geny_3-0.7.1.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.fansi_3-0.4.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.sourcecode_3-0.3.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.get-coursier.coursier-core_2.13-2.0.16.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.get-coursier.coursier-cache_2.13-2.0.16.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.github.alexarchambault.argonaut-shapeless_6.2_2.13-1.2.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.undertow.undertow-core-2.2.3.Final.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.upickle_3-1.6.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.cask-util_3-0.8.3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.google.protobuf.protobuf-java-3.7.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-sbt.util-interface-1.3.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/net.java.dev.jna.jna-5.3.1.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.get-coursier.coursier-util_2.13-2.0.16.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.github.alexarchambault.concurrent-reference-hash-map-1.0.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.github.alexarchambault.windows-ansi.windows-ansi-0.0.3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/io.argonaut.argonaut_2.13-6.2.5.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.chuusai.shapeless_2.13-2.3.3.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.jboss.logging.jboss-logging-3.4.1.Final.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.jboss.xnio.xnio-api-3.8.0.Final.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.jboss.xnio.xnio-nio-3.8.0.Final.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.jboss.threads.jboss-threads-3.1.0.Final.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.ujson_3-1.6.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.upack_3-1.6.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.upickle-implicits_3-1.6.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.castor_3-0.1.8.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.java-websocket.Java-WebSocket-1.5.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.fusesource.jansi.jansi-1.18.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.scala-lang.scala-reflect-2.13.1.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.wildfly.common.wildfly-common-1.5.2.Final.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.wildfly.client.wildfly-client-config-1.0.1.Final.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/com.lihaoyi.upickle-core_3-1.6.0.jar:/home/mp/Projects/scala-repl-pp/lib-staged-works/org.slf4j.slf4j-api-1.7.25.jar")
 
       given Context = ctx
-
-      // TODO: diff settings: classpath, what other setting is set there?
-      // TODO: try out: if using exact same settings, does it work then, or is there some other difference?
-//      println("XXX1 context settings debug start")
-//      println(ctx.settingsState)
-
-      val cp = ctx.settings.classpath.value
-//      println(cp)
-//      cp.split(':')
-//        .sortBy(_.split('/').last)
-//        .map(_.split('/').last)
-//        .sorted
-//        .foreach(println)
-
-//      val settings = ctx.settings.allSettings
-//      println("XXX1 context settings debug end")
 
       if (doCompile(newCompiler, toCompile).hasErrors) {
         Some(ScriptingException("Errors encountered during compilation"))
@@ -80,19 +50,20 @@ class ScriptingDriver(compilerArgs: Array[String], scriptFile: File, scriptArgs:
         val forkJvm = true // TODO get from config
         // TODO refactor for readability
         if (forkJvm) {
-          // TODO check what sbt.Fork does here
           if (verbose) println("forking jvm")
           val args = Seq(
             "-classpath",
             classpath,
             MainClassName
           )
-          println(outDir)
           val p = Process("java", args).run()
           // the next line blocks
           p.exitValue match {
-            case 0 => None // "good case"
-            case nonZero => Some(new AssertionError(s"error while invoking `java ${args.mkString(" ")}`. exit code was $nonZero"))
+            case 0 =>
+              os.remove.all(outDir)
+              None // "good case"
+            case nonZero =>
+              Some(new AssertionError(s"error while invoking `java ${args.mkString(" ")}`. exit code was $nonZero"))
           }
         } else {
           val classpathEntries = ClassPath.expandPath(classpath, expandStar = true).map(Paths.get(_))
