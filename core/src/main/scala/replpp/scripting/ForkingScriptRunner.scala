f6a3931010dd5830e39c62a62e650c5fbfe447f5
Michael Pollmeier <michael@michaelpollmeier.com>
2023-03-20 14:44:06 +0100

    document ForkingScriptRunner

--- core/src/main/scala/replpp/scripting/ForkingScriptRunner.scala
+++ core/src/main/scala/replpp/scripting/ForkingScriptRunner.scala
@@ -4,19 +4,19 @@ import replpp.Config
 
 import sys.process.Process
 
-/** TODO describe why we have this and why this should be the default. Maybe rename to ScriptRunner */
+/** ScriptRunner: executes a script in a separate java process.
+  * Note: there's also a non-forking ScriptRunner, but this one is the default, because some environments have
+  * complex classloader setups that cause issues with the non-forking ScriptRunner - examples include `sbt test`,
+  * `sbt console` and some certain IDEs) */
 object ForkingScriptRunner {
 
   def exec(config: Config): Unit = {
-    // TODO pass on other config options
-    // TODO pass on JAVA_OPTS etc. - and document how to do that
-
     val args = Seq(
       "-classpath",
       replpp.classpath(config),
       "replpp.scripting.ScriptRunner",
     ) ++ config.asJavaArgs
-    if (replpp.verboseEnabled(config)) println(s"forking jvm - executing `java ${args.mkString(" ")}`")
+    if (replpp.verboseEnabled(config)) println(s"executing `java ${args.mkString(" ")}`")
     val exitValue = Process("java", args).run().exitValue()
     assert(exitValue == 0, s"error while invoking `java ${args.mkString(" ")}`. exit code was $exitValue")
   }
