1bec4f237e8f27259868986b41a2aedfdab6a5a8
Michael Pollmeier <michael@michaelpollmeier.com>
2023-04-17 17:17:51 +0200

    WIP

--- core/src/main/scala/replpp/Dependencies.scala
+++ core/src/main/scala/replpp/Dependencies.scala
@@ -7,37 +7,44 @@ import coursier.parse.{DependencyParser, RepositoryParser}
 
 import java.io.File
 import java.nio.file.Path
-import scala.util.{Failure, Try}
+import scala.util.{Failure, Success, Try}
 
 object Dependencies {
 
-  def resolve(coordinates: Seq[String]): Try[Seq[File]] = {
+  def resolve(coordinates: Seq[String], additionalRepositories: Seq[String] = Nil): Try[Seq[File]] = {
     for {
-      dependencies <- parse(coordinates)
+      repositories <- parseRepositories(additionalRepositories)
+      dependencies <- parseDependencies(coordinates)
     } yield {
-      val repository = RepositoryParser.repository("https://shiftleft.jfrog.io/shiftleft/libs-release-local").getOrElse(???)
       coursier.Fetch()
         .withCache(FileCache()) // the default cache throws away the credentials... see PlatformCacheCompanion.scala
-        .addRepositories(repository)
+        .addRepositories(repositories: _*)
         .addDependencies(dependencies: _*)
         .run()
     }
   }
 
-  private def readRepositoryConfiguration(): Try[Seq[Repository]] = {
-    // TODO read coursier property file - default location etc.
-    // TODO add credentials as well
-    ???
+  private def parseRepositories(additionalRepositories: Seq[String]): Try[Seq[Repository]] = {
+    Try {
+      val parseResults = RepositoryParser.repositories(additionalRepositories)
+      if (parseResults.isSuccess) {
+        parseResults.either.getOrElse(???)
+      } else {
+        val failures = parseResults.either.left.getOrElse(???)
+        throw new AssertionError(s"error while trying to parse the following repository coordinates: ${failures.mkString(",")}")
+      }
+    }
   }
 
-  private def parse(coordinates: Seq[String]): Try[Seq[Dependency]] = {
-    val parseResults = coordinates.map(coordinate => DependencyParser.dependency(coordinate, defaultScalaVersion = "3"))
-    val failures = parseResults.collect { case Left(errorMsg) => errorMsg }
+
+  private def parseDependencies(coordinates: Seq[String]): Try[Seq[Dependency]] = {
     Try {
-      if (failures.nonEmpty)
-        throw new AssertionError(s"error while trying to parse the following dependency coordinates: ${failures.mkString(",")}")
-      else {
+      val parseResults = coordinates.map(coordinate => DependencyParser.dependency(coordinate, defaultScalaVersion = "3"))
+      val failures = parseResults.collect { case Left(errorMsg) => errorMsg }
+      if (failures.isEmpty) {
         parseResults.collect { case Right(dependency) => dependency }
+      } else {
+        throw new AssertionError(s"error while trying to parse the following dependency coordinates: ${failures.mkString(",")}")
       }
     }
   }
--- core/src/test/scala/replpp/DependenciesTests.scala
+++ core/src/test/scala/replpp/DependenciesTests.scala
@@ -11,13 +11,14 @@ object DependenciesTests {
     // verify that we can access an artifact that's only available on a separate, password-protected repository
     // note: relies on the local ~/config/coursier/credentials.properties
     // and the private jfrog artifactory in shiftleft.jfrog.io
-    // and the env var `COURSIER_REPOSITORIES=ivy2Local|central|sonatype:releases|jitpack|https://shiftleft.jfrog.io/shiftleft/libs-release-local`
 
-    // TODO set the extra repo in a different way, i.e. not via side effecting system property
-    // TODO set this via a config parameter rather than hardcoded
-    // TODO update the above comment
-//    System.setProperty("coursier.repositories", "ivy2Local|central|sonatype:releases|jitpack|https://shiftleft.jfrog.io/shiftleft/libs-release-local")
-    println(Dependencies.resolve(Seq("io.shiftleft::common:0.3.109")).get)
+    println(
+      Dependencies.resolve(
+//        Seq("io.shiftleftcommon0.3.109"), // TODO should print nice error msg
+        Seq("io.shiftleft::common:0.3.109"),
+        Seq("https://shiftleft.jfrog.io/shiftleft/libs-release-local")
+      ).get
+    )
   }
 
 }
