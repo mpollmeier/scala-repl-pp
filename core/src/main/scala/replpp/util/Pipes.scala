2d105f771b38e6e2ef6c2c83a0b08fb82480dbb1
Michael Pollmeier <michael@michaelpollmeier.com>
2023-07-31 11:46:18 +0200

    WIP

--- core/src/main/scala/replpp/util/Pipes.scala
+++ core/src/main/scala/replpp/util/Pipes.scala
@@ -3,6 +3,7 @@ package replpp.util
 import java.nio.file.{Files, Path, Paths}
 import scala.sys.process.Process
 import scala.util.{Try, Using}
+import System.lineSeparator
 
 object Pipes {
 
@@ -28,12 +29,43 @@ object Pipes {
      */
     def #|(command: String): Unit = {
       val tempFile = Files.createTempFile("replpp-pipes", "txt")
-      val attempt = try {
+      try {
         #>(tempFile)
         import replpp.shaded.os
-        os.proc(Seq("less", tempFile.toAbsolutePath.toString))
-          .call(stdin = os.Inherit, stdout = os.Inherit, stderr = os.Inherit)
-        ()
+        os.proc(Seq(command, tempFile.toString)) .call(stdin = os.Inherit, stdout = os.Inherit, stderr = os.Inherit)
+      } finally {
+        Files.deleteIfExists(tempFile)
+      }
+    }
+  }
+
+  extension (iter: IterableOnce[String]) {
+    private def valueAsString = iter.iterator.mkString(lineSeparator)
+
+    /** Pipe output into file, overriding that file.
+     * Similar to unix' `>` redirection.
+     * We're not using `>` because we don't want to overrode the regular comparison operator... */
+    def #>(outFile: Path): Unit =
+      Files.writeString(outFile, valueAsString)
+
+    /** Pipe output into file, overriding that file.
+     * Similar to unix' `>` redirection.
+     * We're not using `>` because we don't want to overrode the regular comparison operator... */
+    def #>(outFileName: String): Unit =
+      #>(Paths.get(outFileName))
+
+    // TODO implement |>>
+
+    /** Pipe output into a different command.
+     * What actually happens: writes the string value to a temporary file and then passes that to the
+     * given command. In other words, this does not stream the results.
+     */
+    def #|(command: String): Unit = {
+      val tempFile = Files.createTempFile("replpp-pipes", "txt")
+      try {
+        #>(tempFile)
+        import replpp.shaded.os
+        os.proc(Seq(command, tempFile.toString)).call(stdin = os.Inherit, stdout = os.Inherit, stderr = os.Inherit)
       } finally {
         Files.deleteIfExists(tempFile)
       }
