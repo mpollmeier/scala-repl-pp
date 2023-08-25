c6a3607b5aa6a57d51ac040bbf13e79157b6e6c9
Michael Pollmeier <michael@michaelpollmeier.com>
2023-08-25 17:21:10 +0200

    restructure: drop `all`, use `core` by default (#100)
    * by default, use the 'low-impact' `core` with no dependencies
    * drop `all` project, simplify build
    * server mode: combine scopt config parsing with core

--- core/src/main/scala/replpp/Main.scala
+++ core/src/main/scala/replpp/Main.scala
@@ -6,10 +6,7 @@ object Main {
   def main(args: Array[String]): Unit = {
     val config = Config.parse(args)
 
-    if (config.server) {
-      System.err.println("please use `replpp.all.Main` main method to start the server")
-      System.exit(1)
-    } else if (config.scriptFile.isDefined) {
+    if (config.scriptFile.isDefined) {
       ScriptRunner.main(args)
     } else {
       InteractiveShell.run(config)
