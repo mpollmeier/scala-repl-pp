da361202bccbb7a9a490ccc1379f415673cd61fe
Michael Pollmeier <michael@michaelpollmeier.com>
2023-08-31 10:56:23 +0200

    upgrade demo project (#105)

--- core/src/test/resources/demo-project/src/main/scala/stringcalc/Main.scala
+++ core/src/test/resources/demo-project/src/main/scala/stringcalc/Main.scala
@@ -1,20 +1,25 @@
 package stringcalc
 
+import java.nio.file.Files
 import replpp.{Config, InteractiveShell}
 
 object Main {
   @main def startRepl(): Unit = {
-    val predef =
+    val predefFileTmp = Files.createTempFile("scala-repl-pp-demo-project-predef", ".sc")
+    Files.writeString(
+      predefFileTmp,
       s"""import stringcalc._
          |import StringCalculator._
          |
          |def help: Unit = println("try this: `add(One, Two)`")
       """.stripMargin
+    )
+
     InteractiveShell.run(
       Config(
         prompt = Some("stringcalc"),
         greeting = "Welcome to the magical world of string calculation! \nType `help` for help",
-        predefCode = Some(predef),
+        predefFiles = Seq(predefFileTmp),
       )
     )
   }
