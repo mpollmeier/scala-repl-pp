98354b1c84df9cdbe3b25da780a3d0737bf00cc3
Michael Pollmeier <michael@michaelpollmeier.com>
2024-05-29 10:46:05 +0200

    use the given predef files instead of hard-wiring testcode; refactor

--- core/src/main/scala/replpp/InteractiveShell.scala
+++ core/src/main/scala/replpp/InteractiveShell.scala
@@ -2,30 +2,32 @@ package replpp
 
 import replpp.util.SimpleDriver
 
-import java.nio.file.Path
-
 object InteractiveShell {
 
   def run(config: Config): Unit = {
     import config.colors
 
-    // precompile given predef files (if any)
-    val classfiles = new SimpleDriver().compile(
-      replpp.compilerArgs(config),
-      inputFiles = Seq(Path.of("/home/mp/tmp/aaa.scala")),
-      verbose = false
-    ).get
+    val config0 = {
+      if (config.predefFiles.nonEmpty) {
+        val predefClassfiles = new SimpleDriver().compile(
+          replpp.compilerArgs(config),
+          inputFiles = config.predefFiles,
+          verbose = config.verbose
+        ).get
+        config.withAdditionalClasspathEntries(predefClassfiles)
+      } else config
+    }
 
-    val compilerArgs = replpp.compilerArgs(config.withAdditionalClasspathEntries(classfiles))
-    if (verboseEnabled(config))
+    val compilerArgs = replpp.compilerArgs(config0)
+    if (verboseEnabled(config0))
       println(s"compiler arguments: ${compilerArgs.mkString(",")}")
 
     new ReplDriver(
       compilerArgs,
-      onExitCode = config.onExitCode,
-      greeting = config.greeting,
-      prompt = config.prompt.getOrElse("scala"),
-      maxHeight = config.maxHeight
+      onExitCode = config0.onExitCode,
+      greeting = config0.greeting,
+      prompt = config0.prompt.getOrElse("scala"),
+      maxHeight = config0.maxHeight
     ).runUntilQuit()
   }
 
